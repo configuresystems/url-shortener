a50da4649a033e36d1d16166b799fd8156b2a6f8
Johnny M <j.martin@configure.systems>
2014-12-12 04:22:59 +0000

    fixing branch

--- app/api_views.py
+++
@@ -1,5 +0,0 @@
-from app import api
-from app.tni.views import TniUrlListAPI, TniUrlAPI
-
-api.add_resource(TniUrlListAPI, '/api/v1.0/tni', endpoint='tnis')
-api.add_resource(TniUrlAPI, '/api/v1.0/tni/<tni_url>', endpoint='tni')
--- run.py
+++ run.py
@@ -2,10 +2,14 @@
 
 # import our application
 from app import app
-# import a flask manager for multi-threaded
+
+# The Flask-Actions extension provides support for writing external actions in
+# Flask. This includes running a development server, a customized python
+# shell, a fastcgi server like django
+# https://pythonhosted.org/Flask-Actions/
 from flask.ext.actions import Manager
 
 if __name__ == '__main__':
-    # instantiate Manager as a wrapper for our app
+    # Start up our application when run.py is executed
     manager = Manager(app)
     manager.run()
